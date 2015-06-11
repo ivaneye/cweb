0be9a795cdfbb6356a36ed57776d0dd07c5c53cc
ivanpig <ivan.pig@foxmail.com>
2015-06-11 17:08:54 +0800

    添加yesql

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [ring/ring-defaults "0.1.2"]
                            [compojure "1.3.4"]
-                           [selmer "0.8.2"]]
+                           [selmer "0.8.2"]
+                           [yesql "0.5.0-rc2"]
+                           [mysql/mysql-connector-java "5.1.32"]]
             :plugins [[lein-ring "0.8.11"]]
             :ring {:handler cweb.core/app})
